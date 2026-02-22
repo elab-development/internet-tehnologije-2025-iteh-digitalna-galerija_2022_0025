3d4f0b23f52a211e6961eb838a3efc467e074672
jana <janaostojic22@gmail.com>
2026-02-22 12:43:53 +0100

    Dodavanje WeatherWidget komponente i izmena konfiguracije

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,3 +4,10 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.svg";
+
+interface ImportMetaEnv {
+	readonly VITE_OPENWEATHER_API_KEY: string;
+}
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
